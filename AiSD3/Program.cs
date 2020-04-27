40b50b08c5d5981f1668ca20109623bba092f21b
RomanovEgor99 <romanov.egor2010.ru@gmail.com>
2020-04-27 13:21:41 +0400

    Добавьте файлы проекта.

---
+++ AiSD3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiSD3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
