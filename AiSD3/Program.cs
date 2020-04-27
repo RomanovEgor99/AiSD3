b447375fc4cf4a6ab88d23dc781069f3a0c29166
RomanovEgor99 <romanov.egor2010.ru@gmail.com>
2020-04-27 13:25:21 +0400

    2 laba

--- AiSD3/Program.cs
+++ AiSD3/Program.cs
@@ -10,6 +10,118 @@ namespace AiSD3
     {
         static void Main(string[] args)
         {
+
+            Student student = new Student();
+            Student[] arrayOfStudens = new Student[4];
+            student.InputInfo();
+            student.InputUintArray();
+            student.OutputInfo();
+            student.OutputUintArray();
+            for (int i = 0; i < arrayOfStudens.Length; i++)
+            {
+                arrayOfStudens[i].InputInfo();
+                arrayOfStudens[i].InputUintArray();
+
+            }
+
+            for (int i = 0; i < arrayOfStudens.Length; i++)
+            {
+
+                arrayOfStudens[i].OutputInfo();
+                arrayOfStudens[i].OutputUintArray();
+            }
+
+
+
+
+            Console.ReadLine();
+
+
         }
+
+
+
+
     }
+
+    struct Student
+    {
+        public Char[] lastName;
+        public Char[] firstName;
+        public int id;
+        public uint[] uintNum;
+
+        public Student(Char[] arrLastName, Char[] arrFirstName, int studentId, uint[] arrUint)
+        {
+            lastName = arrLastName;
+            firstName = arrFirstName;
+            id = studentId;
+            uintNum = arrUint;
+
+        }
+
+
+        public void InputUintArray()
+        {
+            uint[] newUint = new uint[4];
+            Console.WriteLine("Ввод массива беззнаковых чисел.");
+
+
+            for (int k = 0; k < 4; k++)
+            {
+
+                Console.WriteLine("Введите число №{0}:", k + 1);
+                newUint[k] = Convert.ToUInt32(Console.ReadLine());
+
+            }
+
+            uintNum = newUint;
+
+        }
+        public void OutputUintArray()
+        {
+            Console.WriteLine("Массив беззнаковых чисел");
+            for (int k = 0; k < uintNum.Length; k++)
+            {
+                Console.Write("{0}", uintNum[k]);
+
+            }
+            Console.WriteLine("\n");
+        }
+        public void InputInfo()
+        {
+            Console.WriteLine("Введите Фамилию студента:");
+            string lastNameStr = Console.ReadLine();
+
+            lastName = lastNameStr.ToCharArray(0, lastNameStr.Length);
+
+            Console.WriteLine("Введите имя студента");
+            string firstNameStr = Console.ReadLine();
+
+            firstName = firstNameStr.ToCharArray(0, firstNameStr.Length);
+
+            Console.WriteLine("Введите id студента:");
+            id = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void OutputInfo()
+        {
+            Console.Write("Фамилия: ");
+            for (int i = 0; i < lastName.Length; i++)
+                Console.Write(lastName[i]);
+            Console.WriteLine();
+
+            Console.Write("Имя: ");
+            for (int i = 0; i < firstName.Length; i++)
+                Console.Write(firstName[i]);
+            Console.WriteLine();
+            Console.WriteLine("Id: {0}", id);
+
+
+        }
+
+
+    }
+
 }
+
